a33d4ab70d9786d7086e555fccb6e7d9936ffbe3
BOUDI Ludovic <ludovic.boudi@cisapp.com>
2016-11-25 11:51:53 +0100

    update de ma lib perso

--- MyPHPLib.php
+++ MyPHPLib.php
@@ -95,6 +95,47 @@ function SshCmd($host, $login, $mdp, $command)
 		}
 }
 
+function ScpPut($host, $login, $mdp, $src, $dest)
+{
+	if(!function_exists("ssh2_connect")) die ("function ssh2_connect does not exist");
+	if (!($con = ssh2_connect($host, 22))) 
+		{
+			return "echec connexion";
+		}
+	else
+		{
+			if (!ssh2_auth_password($con, $login, $mdp))
+			{
+				return "echec authentification";
+			}
+			else
+			{
+				ssh2_scp_send($con, $src, $dest, 0644);
+				return 0;
+			}
+		}
+}
+
+function ScpGet($host, $login, $mdp, $src, $dest)
+{
+	if(!function_exists("ssh2_connect")) die ("function ssh2_connect does not exist");
+	if (!($con = ssh2_connect($host, 22))) 
+		{
+			return "echec connexion";
+		}
+	else
+		{
+			if (!ssh2_auth_password($con, $login, $mdp))
+			{
+				return "echec authentification";
+			}
+			else
+			{
+				ssh2_scp_recv($con, $src, $dest);
+				return 0;
+			}
+		}
+}
 
 
 
